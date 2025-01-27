4bf1db92616ab353739cc61b1fd17538075847ca
litingyes <luz.liting@gmail.com>
2025-01-27 10:53:31 +0800

    feat(editor): init

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,30 +1,11 @@
-<script setup lang="ts">
-import Versions from './components/Versions.vue'
+<script lang="ts" setup>
+import MaskyEditor from './editor/index.vue'
 
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+defineOptions({
+  name: 'MaskyMain',
+})
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg">
-  <div class="creator">
-    Powered by electron-vite
-  </div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
-  </div>
-  <p class="tip">
-    Please try pressing <code>F12</code> to open the devTool
-  </p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
+  <MaskyEditor />
 </template>
---
+++ src/renderer/src/editor/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import StarterKit from '@tiptap/starter-kit'
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+
+defineOptions({
+  name: 'MaskyEditor',
+})
+
+const editor = useEditor({
+  content: '<p>Masky editor 🎉</p>',
+  extensions: [StarterKit],
+})
+</script>
+
+<template>
+  <EditorContent :editor="editor" />
+</template>
