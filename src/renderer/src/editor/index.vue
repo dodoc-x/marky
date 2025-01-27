e56086af5d7ff3f64a83d34c6ab0d03cbbae9a8a
litingyes <luz.liting@gmail.com>
2025-01-27 23:40:39 +0800

    feat(editor): add basic exts

--- src/renderer/src/editor/index.vue
+++ src/renderer/src/editor/index.vue
@@ -1,4 +1,11 @@
 <script lang="ts" setup>
+import Link from '@tiptap/extension-link'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import StarterKit from '@tiptap/starter-kit'
 import { EditorContent, useEditor } from '@tiptap/vue-3'
 
@@ -8,7 +15,25 @@ defineOptions({
 
 const editor = useEditor({
   content: '<p>Masky editor 🎉</p>',
-  extensions: [StarterKit],
+  extensions: [
+    StarterKit,
+
+    // node
+    Table,
+    TableRow,
+    TableHeader,
+    TableCell,
+    TaskList,
+    TaskItem,
+
+    // mark
+    Link,
+  ],
+  editorProps: {
+    attributes: {
+      class: 'py-4 px-6 focus:outline-none',
+    },
+  },
 })
 </script>
 
